f339793d4e8a1c1cd3da3db50e82f433a21546b4
Digitalroot <digitalroot@gmail.com>
2026-09-24 07:37:52 -0700

    Updated

--- src/Digitalroot.Modding.Framework/Common/AssemblyInfo.cs
+++ src/Digitalroot.Modding.Framework/Common/AssemblyInfo.cs
@@ -3,12 +3,12 @@
   // ReSharper disable once UnusedType.Global
   public static class AssemblyInfo
   {
-    public const string Title = "Digitalroot.Valheim.Common";
-    public const string Description = "Digitalroot Valheim Common";
+    public const string Title = "DMF";
+    public const string Description = "Digitalroot Valheim Modding Framework";
     public const string Configuration = "";
     public const string Company = "Digitalroot Technologies";
     public const string Product = "Digitalroot Valheim Mods";
-    public const string Copyright = "Copyright © Digitalroot Technologies 2021 - 2024";
+    public const string Copyright = "Copyright © Digitalroot Technologies 2021 - 2026";
     public const string Trademark = "";
     public const string Culture = "en-US";
     public const string Version = "0.0.1";
